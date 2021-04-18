f6780fe653e84e10eaa56ab6d7e63067019b0558
Manish-DE <manishk.dhn@gmail.com>
2021-04-18 13:51:58 -0400

    BookModel

---
+++ Web API Core/Console To Web API/ConsoleToWebAPI/ConsoleToWebAPI/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [ApiController]
+    [Route("Test/[action]")]
+    public class TestController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello from Get";
+        }
+        public string Get1()
+        {
+            return "Hello from Get1";
+        }
+    }
+}
---
+++ Web API Core/Console To Web API/ConsoleToWebAPI/ConsoleToWebAPI/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI
+{
+    public class Startup
+    {
+        // to inject any service in this application 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        //To register middelware 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello from Run");
+            //});
+            app.Use(async (Context, next) =>
+            {
+                await Context.Response.WriteAsync("Hello from Use-1 1 \n");
+                await next();
+                await Context.Response.WriteAsync("Hello from Use-1 2 \n");
+
+            });
+            app.Use(async (Context, next) =>
+            {
+                await Context.Response.WriteAsync("Hello from Use-2 1 \n");
+                await next();
+                await Context.Response.WriteAsync("Hello from Use-2 2 \n");
+
+            });
+            app.Use(async (Context, next) =>
+            {
+                await Context.Response.WriteAsync("Request Complete 1 \n");
+
+            });
+            //app.Run(async Context =>
+            //{
+            //    await Context.Response.WriteAsync("Hello from Run \n");
+            //});
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage(); //middleware
+            }
+            app.UseRouting();    //middleware
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapControllers();    //middleware
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello from new WebAPI app");
+                //});
+                //endpoints.MapGet("/test", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello from new WebAPI app Test");
+                //});
+            });
+        }
+    }
+    
+}
